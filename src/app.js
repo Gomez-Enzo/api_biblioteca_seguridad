d3871e7d2b55b1913011ee85f6f342dd7c95f03d
Frocz <gomezenzo1128@gmail.com>
2024-01-06 15:43:57 -0300

    install dotenv

--- src/app.js
+++ src/app.js
@@ -1,16 +1,21 @@
 const express = require("express");
+
+require('dotenv').config();
+
 const errorHandler = require("./middlewares/errorHandler");
 const routeLibros = require("./routes/libros");
 const {auth} = require("express-oauth2-jwt-bearer");
 
+
 const autenticacion = auth({
-    audience: "http://localhost:3000/api/productos",
-    issuerBaseURL: "https://dev-utn-frc-iaew.auth0.com/",
+    audience: process.env.OAUTH_AUDIENCE,
+    issuerBaseURL: process.env.OAUTH_URL,
     tokenSigningAlg: "RS256",
 });
     
 const app = express();
 app.use(express.json());
+
 app.use('/libros', autenticacion, routeLibros);
 app.use(errorHandler);
 
